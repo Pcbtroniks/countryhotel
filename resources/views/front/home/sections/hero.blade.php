00bb7717d2d7aec61cbecc746304b2ae28aef41d
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-01-16 16:56:04 -0600

    Fix\ Logo Hero

--- resources/views/front/home/sections/hero.blade.php
+++ resources/views/front/home/sections/hero.blade.php
@@ -1,6 +1,7 @@
 <section>
-    <div class="hero-mobile-bg bg-cover w-full h-screen">
+    <div class="hero-bg bg-cover w-full h-screen">
         <div class="text-white text-center flex flex-col justify-center gap-8 border h-full">
+            <img class="w-64 mx-auto" src="{{ asset('assets/log-alt.png') }}" alt="logo alt">
             <h2 class="text-2xl font-bold px-12">
                 UNA EXCELENTE UBICACIÓN Y CÓMODA ESTADÍA
             </h2>
