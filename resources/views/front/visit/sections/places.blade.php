e6689e04da2e301756f7e665661a701f1841a3dc
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-16 13:14:22 -0600

    Fix\ Places Map W & H

--- resources/views/front/visit/sections/places.blade.php
+++ resources/views/front/visit/sections/places.blade.php
@@ -1,41 +1,41 @@
 <section>
+    <div class="lg:p-20 p-0 bg-main-light">
+        <div class="shadow-card flex md:grid md:grid-cols-2 flex-col-reverse max-h-96">
 
-    <div class="space-y"></div>
-    <div class="bg-main-light flex md:grid md:grid-cols-2 flex-col-reverse">
+            <div class="px-10 md:mx-auto my-11">
+                <h2 class="font-bold text-xs uppercase mb-10">
+                    Country Hotel & Suites
+                </h2>
 
-        <div class="px-10 md:mx-auto my-11">
-            <h2 class="font-bold text-xs uppercase mb-10">
-                Country Hotel & Suites
-            </h2>
+                <h3 class="font-bold text-3xl mb-10">EN GUADALAJARA</h3>
 
-            <h3 class="font-bold text-3xl mb-10">EN GUADALAJARA</h3>
+                <p class="text-sm leading-6">
+                    Country Hotel & Suites abre sus puertas para recibirte con la mejor atención. Ya sea que viajes por placer o negocios, buscaremos que tengas una cómoda estadía. Estamos comprometidos con ofrecer un servicio cálido y personalizado a cada uno de nuestros huéspedes.
+                </p>
 
-            <p class="text-sm leading-6">
-                Country Hotel & Suites abre sus puertas para recibirte con la mejor atención. Ya sea que viajes por placer o negocios, buscaremos que tengas una cómoda estadía. Estamos comprometidos con ofrecer un servicio cálido y personalizado a cada uno de nuestros huéspedes.
-            </p>
+                <div class="space-y"></div>
+                <ul class="hidden">
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> 77 Habitaciones</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Wi-Fi Gratis</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Restaurante</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Recepción 24 Hrs.</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Seguridad Privada</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Periódicos gratuitos en el lobby</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Estacionamiento gratis</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Traslado al aeropuerto</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Personal multilingüe</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Artículos de tocador gratuitos</li>
+                </ul>
 
-            <div class="space-y"></div>
-            <ul class="hidden">
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> 77 Habitaciones</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Wi-Fi Gratis</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Restaurante</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Recepción 24 Hrs.</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Seguridad Privada</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Periódicos gratuitos en el lobby</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Estacionamiento gratis</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Traslado al aeropuerto</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Personal multilingüe</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Artículos de tocador gratuitos</li>
-            </ul>
-
-            <div class="my-12 hidden">
-                <button class="w-36 h-12 font-bold bg-main text-white text-sm">ver todos</button>
+                <div class="my-12 hidden">
+                    <button class="w-36 h-12 font-bold bg-main text-white text-sm">ver todos</button>
+                </div>
             </div>
-        </div>
 
-        <div>
-            <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/u/0/embed?mid=1ihlqwTf_AppFt14DohJk0loynGZe97c&ehbc=2E312F" width="640" height="480"></iframe>
-        </div>
+            <div>
+                <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/u/0/embed?mid=1ihlqwTf_AppFt14DohJk0loynGZe97c&ehbc=2E312F" width="640" height="480"></iframe>
+            </div>
 
+        </div>
     </div>
 </section>
