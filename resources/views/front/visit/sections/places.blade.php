08b8d22a561a98895ae7e0691f66da4c0ca9c44b
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-16 13:00:17 -0600

    Update\ CountryHotel Map on visita-guadalajara Page

--- resources/views/front/visit/sections/places.blade.php
+++ resources/views/front/visit/sections/places.blade.php
@@ -34,8 +34,7 @@
         </div>
 
         <div>
-            <iframe class="w-full max-h-[630px] h-full object-cover hidden" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Av.%20Circ.%20Jorge%20%C3%81lvarez%20del%20Castillo%201482%2C%20Chapultepec%20Country%2C%2044620&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near" title="Av. Circ. Jorge Álvarez del Castillo 1482, Chapultepec Country, 44620" aria-label="Av. Circ. Jorge Álvarez del Castillo 1482, Chapultepec Country, 44620"></iframe>
-            <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/embed?mid=1au3-cozD0FQQcgUY_H2Sml3jKSYjV2c&ehbc=2E312F" width="640" height="480"></iframe>
+            <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/u/0/embed?mid=1ihlqwTf_AppFt14DohJk0loynGZe97c&ehbc=2E312F" width="640" height="480"></iframe>
         </div>
 
     </div>
